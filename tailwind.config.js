7167a27eced7761e5e1f9dd224d1f9469925143b
Kiprono Deno <boratechlife@gmail.com>
2021-01-24 18:08:26 +0300

    finish up on footer and headers

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,27 @@ module.exports = {
             },
 
         },
+
+        customForms: theme => ({
+            default: {
+                input: {
+                    borderRadius: theme('borderRadius.lg'),
+                    backgroundColor: theme('colors.gray.200'),
+                    '&:focus': {
+                        backgroundColor: theme('colors.white'),
+                    }
+                },
+                select: {
+                    borderRadius: theme('borderRadius.lg'),
+                    boxShadow: theme('boxShadow.default'),
+                },
+                checkbox: {
+                    width: theme('spacing.6'),
+                    height: theme('spacing.6'),
+                    borderColor: theme('colors.primary')
+                },
+            },
+        })
     },
     variants: {
         extend: {},
